b420553c5f93874ba201deaa2c351aeb3e557ecd
Subaraki <robbeenaxel@hotmail.com>
2021-10-29 00:43:33 +0200

    Config Knee default
    set to 10

--- src/main/java/subaraki/badbone/mod/ConfigData.java
+++ src/main/java/subaraki/badbone/mod/ConfigData.java
@@ -11,7 +11,7 @@ public class ConfigData {
     public static final ForgeConfigSpec CLIENT_SPEC;
     public static int frequencyHurt = 24_000 / 3;
     public static int chanceHurt = 10;
-    public static int frequencyKnee = 1;
+    public static int frequencyKnee = 10;
     public static int frequencyArthritis = 24_000;
     public static int frequencyEyes = 24_000;
 
@@ -55,7 +55,7 @@ public class ConfigData {
             builder.push("general");
             freqHurt = builder.comment("frequency at which the inventory gets checked for backpain (values in ticks)").defineInRange("backpain_frequency", 24_000 / 3, 1, 24_000 * 10);
             chanHurt = builder.comment("chance to have back pain applied when inventory is too full").defineInRange("backpain_chance", 10, 1, 1000);
-            freqKnee = builder.comment("frequency at which your knees can go bad when falling (values in ticks)").defineInRange("bad_knees_frequency", 1, 1, 1000);
+            freqKnee = builder.comment("frequency at which your knees can go bad when falling (values in ticks)").defineInRange("bad_knees_frequency", 10, 1, 1000);
             freqArth = builder.comment("frequency at which arthritis will occur when using items (values in ticks)").defineInRange("arthritis_frequency", 24_000 / 2, 1, 24_000 * 10);
             freqEyes = builder.comment("frequency at which your eyes will go bad (values in ticks)").defineInRange("blurry_eyes_frequency", 24_000 * 3, 1, 24_000 * 30);
 
