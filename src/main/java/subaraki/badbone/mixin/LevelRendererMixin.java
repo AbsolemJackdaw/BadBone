ced65087a5d923040afcc747a67bd76d88c39226
Subaraki <robbeenaxel@hotmail.com>
2021-10-31 15:56:55 +0100

    added far and short sightedness

--- src/main/java/subaraki/badbone/mixin/LevelRendererMixin.java
+++ src/main/java/subaraki/badbone/mixin/LevelRendererMixin.java
@@ -1,127 +1,122 @@
 package subaraki.badbone.mixin;
 
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import com.mojang.blaze3d.pipeline.TextureTarget;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.*;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.mojang.blaze3d.pipeline.RenderTarget;
-import com.mojang.blaze3d.pipeline.TextureTarget;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Matrix4f;
-
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.EffectInstance;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.PostChain;
-import net.minecraft.client.renderer.PostPass;
 import subaraki.badbone.client.init.BadBoneShaders;
 import subaraki.badbone.client.shader.ExtendedPostChain;
 
 @Mixin(LevelRenderer.class)
-public class LevelRendererMixin
-{
-	private static final Matrix4f
-		PROJECTION_INVERSE = new Matrix4f(),
-		VIEW_INVERSE = new Matrix4f();
+public class LevelRendererMixin {
+    private static final Matrix4f
+            PROJECTION_INVERSE = new Matrix4f(),
+            VIEW_INVERSE = new Matrix4f();
 
-	@Shadow
-	private PostChain transparencyChain;
+    private static final float[]
+            SHORT = new float[]{4.0f, 20.0f, 0.0f, 32.0f},
+            FAR = new float[]{0.0f, 80.f, 24.0f, 0.0f};
 
-	@Unique
-	private RenderTarget depthCopy;
+    @Shadow
+    private PostChain transparencyChain;
 
-	@Unique
-	private RenderTarget lazyDepthCopy()
-	{
-		Minecraft mc = Minecraft.getInstance();
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+    @Unique
+    private RenderTarget depthCopy;
 
-		if(this.depthCopy == null)
-		{
-			this.depthCopy = new TextureTarget(mc.getWindow().getWidth(), mc.getWindow().getHeight(), true, Minecraft.ON_OSX);
-		}
+    @Unique
+    private RenderTarget lazyDepthCopy() {
+        Minecraft mc = Minecraft.getInstance();
 
-		return this.depthCopy;
-	}
+        if (this.depthCopy == null) {
+            this.depthCopy = new TextureTarget(mc.getWindow().getWidth(), mc.getWindow().getHeight(), true, Minecraft.ON_OSX);
+        }
 
-	@Inject(
-		at = @At(
-			value = "invoke",
-			target = "Lnet/minecraft/client/renderer/PostChain;process(F)V",
-			ordinal = 1),
-		method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lcom/mojang/math/Matrix4f;)V")
-	private void renderLevelPreTransparency(PoseStack mtx, float frameTime, long nanoTime, boolean renderOutline, Camera camera, GameRenderer gameRenderer, LightTexture light, Matrix4f projMat, CallbackInfo ci)
-	{
-		// This is required because apparently the transparency (fabulous) shader decides it's a wonderful idea to nuke the depth buffer!!
-		// FIXME this is better than nothing, but the depth of particles and other things is lost this way
+        return this.depthCopy;
+    }
 
-		Minecraft mc = Minecraft.getInstance();
+    @Inject(
+            at = @At(
+                    value = "invoke",
+                    target = "Lnet/minecraft/client/renderer/PostChain;process(F)V",
+                    ordinal = 1),
+            method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lcom/mojang/math/Matrix4f;)V")
+    private void renderLevelPreTransparency(PoseStack mtx, float frameTime, long nanoTime, boolean renderOutline, Camera camera, GameRenderer gameRenderer, LightTexture light, Matrix4f projMat, CallbackInfo ci) {
+        // This is required because apparently the transparency (fabulous) shader decides it's a wonderful idea to nuke the depth buffer!!
+        // FIXME this is better than nothing, but the depth of particles and other things is lost this way
 
-		RenderTarget main = mc.getMainRenderTarget();
-		this.lazyDepthCopy();
+        Minecraft mc = Minecraft.getInstance();
 
-		if(this.depthCopy.width != main.width || this.depthCopy.height != main.height)
-		{
-			this.depthCopy.resize(main.width, main.height, false);
-		}
+        RenderTarget main = mc.getMainRenderTarget();
+        this.lazyDepthCopy();
 
-		this.depthCopy.setClearColor(0f, 0f, 0f, 0f);
-		this.depthCopy.copyDepthFrom(main);
-	}
+        if (this.depthCopy.width != main.width || this.depthCopy.height != main.height) {
+            this.depthCopy.resize(main.width, main.height, false);
+        }
 
-	@Inject(
-		at = @At(
-			value = "TAIL"),
-		method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lcom/mojang/math/Matrix4f;)V")
-	private void renderLevelPostTransparency(PoseStack mtx, float frameTime, long nanoTime, boolean renderOutline, Camera camera, GameRenderer gameRenderer, LightTexture light, Matrix4f projMat, CallbackInfo ci)
-	{
-		this.applyBlur(mtx, frameTime);
-	}
+        this.depthCopy.setClearColor(0f, 0f, 0f, 0f);
+        this.depthCopy.copyDepthFrom(main);
+    }
 
-	@Unique
-	private void applyBlur(PoseStack mtx, float frameTime)
-	{
-		Minecraft mc = Minecraft.getInstance();
+    @Inject(
+            at = @At(
+                    value = "TAIL"),
+            method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lcom/mojang/math/Matrix4f;)V")
+    private void renderLevelPostTransparency(PoseStack mtx, float frameTime, long nanoTime, boolean renderOutline, Camera camera, GameRenderer gameRenderer, LightTexture light, Matrix4f projMat, CallbackInfo ci) {
+        this.applyBlur(mtx, frameTime);
+    }
 
-		// if(!mc.player.hasEffect(BadBoneEffects.BLIND.get()) || !PlayerUpdateEvent.isInSurvivalMode(mc.player) || mc.player.getInventory().getArmor(3).getItem() instanceof GlassesItem)
-		{
-			// return;
-		}
+    @Unique
+    private void applyBlur(PoseStack mtx, float frameTime) {
+        Minecraft mc = Minecraft.getInstance();
 
-		ExtendedPostChain chain = BadBoneShaders.INSTANCE.getBlur();
+        // if(!mc.player.hasEffect(BadBoneEffects.BLIND.get()) || !PlayerUpdateEvent.isInSurvivalMode(mc.player) || mc.player.getInventory().getArmor(3).getItem() instanceof GlassesItem)
+        {
+            // return;
+        }
 
-		if(chain == null)
-		{
-			return;
-		}
+        ExtendedPostChain chain = BadBoneShaders.INSTANCE.getBlur();
 
-		PROJECTION_INVERSE.load(RenderSystem.getProjectionMatrix());
-		PROJECTION_INVERSE.invert();
-	
-		VIEW_INVERSE.load(mtx.last().pose());
-		VIEW_INVERSE.invert();
+        if (chain == null) {
+            return;
+        }
 
-		for(PostPass pass : chain.passes)
-		{
-			EffectInstance shader = pass.getEffect();
-	
-			shader.safeGetUniform("ProjInverseMat").set(PROJECTION_INVERSE);
-			shader.safeGetUniform("ViewInverseMat").set(VIEW_INVERSE);
-		}
+        PROJECTION_INVERSE.load(RenderSystem.getProjectionMatrix());
+        PROJECTION_INVERSE.invert();
 
-		// if fabulous is enabled, restore our saved depth
-		if(this.transparencyChain != null)
-		{
-			mc.getMainRenderTarget().copyDepthFrom(depthCopy);
-		}
+        VIEW_INVERSE.load(mtx.last().pose());
+        VIEW_INVERSE.invert();
 
-		chain.process(frameTime);
-		mc.getMainRenderTarget().bindWrite(false);
-	}
+        for (PostPass pass : chain.passes) {
+            EffectInstance shader = pass.getEffect();
+
+            shader.safeGetUniform("ProjInverseMat").set(PROJECTION_INVERSE);
+            shader.safeGetUniform("ViewInverseMat").set(VIEW_INVERSE);
+            long l = minecraft.player.getUUID().getMostSignificantBits() % 100;
+            boolean flag = l < 50;
+            shader.safeGetUniform("SightedType").set(flag ? SHORT : FAR);
+
+        }
+
+        // if fabulous is enabled, restore our saved depth
+        if (this.transparencyChain != null) {
+            mc.getMainRenderTarget().copyDepthFrom(depthCopy);
+        }
+
+        chain.process(frameTime);
+        mc.getMainRenderTarget().bindWrite(false);
+    }
 }
--- src/main/resources/assets/badbone/shaders/program/blur.fsh
+++ src/main/resources/assets/badbone/shaders/program/blur.fsh
@@ -8,6 +8,9 @@ uniform mat4 ViewInverseMat;
 
 uniform vec2 BlurDir;
 
+uniform vec4 SightedType;
+
+
 in vec2 texCoord;
 in vec2 oneTexel;
 
@@ -18,66 +21,66 @@ out vec4 fragColor;
 
 vec3 playerSpace(vec2 uv, float depth)
 {
-	vec3 ndc = vec3(uv, depth) * 2.0 - 1.0;
-	vec4 posCS = vec4(ndc, 1.0);
-	vec4 posVS = ProjInverseMat * posCS;
-	posVS /= posVS.w;
-	vec4 posPS = ViewInverseMat * posVS;
-	return posPS.xyz;
+    vec3 ndc = vec3(uv, depth) * 2.0 - 1.0;
+    vec4 posCS = vec4(ndc, 1.0);
+    vec4 posVS = ProjInverseMat * posCS;
+    posVS /= posVS.w;
+    vec4 posPS = ViewInverseMat * posVS;
+    return posPS.xyz;
 }
 
 float remap(float v, float minOld, float maxOld, float minNew, float maxNew)
 {
-	return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
+    return minNew + (v - minOld) * (maxNew - minNew) / (maxOld - minOld);
 }
 
 float gaussian(int x)
 {
-	float sigma = 25.0; // standard deviation. No idea what this actually does
-	float sigmaSq = sigma * sigma;
+    float sigma = 20.0;// standard deviation. No idea what this actually does
+    float sigmaSq = sigma * sigma;
 
-	// 1 / sqrt(2 * PI) = 0.398942
+    // 1 / sqrt(2 * PI) = 0.398942
 
-	return 0.398942 / sigma * exp(float(-x * x) / (2.0 * sigmaSq));
+    return 0.398942 / sigma * exp(float(-x * x) / (2.0 * sigmaSq));
 }
 
 void main()
 {
-	// Convert depth to distance
+    // Convert depth to distance
 
-	float depth = texture(DiffuseDepthSampler, texCoord).r;
-	vec3 posPS = playerSpace(texCoord, depth);
-	float dstToSurface = length(posPS);
-	
-	
-	// Init variables
+    float depth = texture(DiffuseDepthSampler, texCoord).r;
+    vec3 posPS = playerSpace(texCoord, depth);
+    float dstToSurface = length(posPS);
 
-	vec3 color = vec3(0.0);
 
-	// Set kernel radius (blur strength) to be dependent on depth
-	// So at 0 distance kernel radius = 0 (no blur) and at 8 (or more) distance it is 16
+    // Init variables
 
-	int kernelRadius = int(remap(clamp(dstToSurface, 4.0, 8.0), 4.0, 8.0, 0.0, 32.0));
+    vec3 color = vec3(0.0);
 
-	if(kernelRadius == 0)
-	{
-		color = texture(DiffuseSampler, texCoord).rgb;
-	}
-	else
-	{
-		float kernelSum = 0.0;
-		
-		for(int x = -kernelRadius; x <= kernelRadius; ++x)
-		{
-			float gauss = gaussian(x);
-			kernelSum += gauss;
-			color += gauss * texture(DiffuseSampler, texCoord + oneTexel * BlurDir * x, 0.0).rgb;
-		}
-		
-		color /= kernelSum;
-	}
+    // Set kernel radius (blur strength) to be dependent on depth
+    // So at 0 distance kernel radius = 0 (no blur) and at 8 (or more) distance it is 16
 
-	fragColor = vec4(color, 1.0);
-	
-	gl_FragDepth = depth;
+    int kernelRadius = int(remap(clamp(dstToSurface, SightedType.x, SightedType.y), SightedType.x, SightedType.y, SightedType.z, SightedType.w));
+
+    if (kernelRadius == 0)
+    {
+        color = texture(DiffuseSampler, texCoord).rgb;
+    }
+    else
+    {
+        float kernelSum = 0.0;
+
+        for (int x = -kernelRadius; x <= kernelRadius; ++x)
+        {
+            float gauss = gaussian(x);
+            kernelSum += gauss;
+            color += gauss * texture(DiffuseSampler, texCoord + oneTexel * BlurDir * x, 0.0).rgb;
+        }
+
+        color /= kernelSum;
+    }
+
+    fragColor = vec4(color, 1.0);
+
+    gl_FragDepth = depth;
 }
